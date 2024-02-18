8de7f6c9f70ff8f8ac712a131990787b5195132a
Ivan <79423340+Ivansallas@users.noreply.github.com>
2024-02-18 14:33:35 -0300

    Correção
    feita a correção

---
+++ aula01.py
@@ -0,0 +1,10 @@
+# Fazer a instalação de repositório do GitHub
+print("Bem-vindo à Aula de PROGRAMAÇÃO BACK END")
+print("Nesta aula01 vamos aprender funcionalidade do GitHub")
+
+# Inserindo informação
+gitHub = input("Você conseguiu clonar? (sim/não) ")
+if gitHub.lower() == "sim":
+    print("OK, clonado com sucesso!")
+else:
+    print("Tente novamente.")
