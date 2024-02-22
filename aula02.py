facb42f42373ee6ed7c584735129f657afd13f71
kiss_ivan@hotmail.com <79423340+Ivansallas@users.noreply.github.com>
2024-02-22 08:56:04 -0300

    adicionei site

--- aula02.py
+++ aula02.py
@@ -1,4 +1,6 @@
+# https://github.com/shiftkey/desktop/releases/tag/release-3.3.8-linux2
 # Fazer a instalação de repositório do GitHub
+
 print("Bem-vindo à Aula de PROGRAMAÇÃO BACK END")
 print("Nesta aula02 vamos aprender funcionalidade do GitHub")
 
